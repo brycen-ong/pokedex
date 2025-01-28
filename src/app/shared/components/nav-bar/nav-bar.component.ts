bf8404395ddafa7c16ce39997dd563291ccd67a2
Brycen <brycenong321@gmail.com>
2025-01-28 11:41:01 +0800

    updated nav bar

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
     MatToolbarModule,
+    SvgIconComponent,
+    RouterLink,
+    MatButtonModule
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
-
-}
+export class NavBarComponent {}

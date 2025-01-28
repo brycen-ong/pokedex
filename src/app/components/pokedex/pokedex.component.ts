66ba33d36c9dad58c486a840aceef92918369f29
Brycen Ong <brycenong321@gmail.com>
2025-01-28 21:45:28 +0800

    updated pokedex component

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { PokemonService } from '../../services/pokemon.service';
+import { firstValueFrom } from 'rxjs';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pokedex',
@@ -11,10 +14,21 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    FormsModule,
   ],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.scss'
 })
 export class PokedexComponent {
+  pokemonInput: string = '';
 
+  constructor(
+    private pokemonService: PokemonService,
+  ) {}
+
+  async getPokemon(): Promise<void> {
+    console.log(this.pokemonInput);
+    const res = await firstValueFrom(this.pokemonService.getPokemonData(this.pokemonInput));
+    console.log(res);
+  }
 }

6a89ec1eeea0eb19fe837417d79bf879e312cb08
Brycen <brycenong321@gmail.com>
2025-01-28 11:41:53 +0800

    added svg icon component

---
+++ src/app/shared/components/svg-icon/svg-icon.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-svg-icon',
+  imports: [
+    MatIconModule,
+  ],
+  templateUrl: './svg-icon.component.html',
+  styleUrl: './svg-icon.component.scss'
+})
+export class SvgIconComponent {
+  @Input() icon = '';
+  @Input() size: 's' | 'm' | 'l' | 'xl' = 'm';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'pokeball',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/svg/pokeball.svg')
+    );
+  }
+
+}

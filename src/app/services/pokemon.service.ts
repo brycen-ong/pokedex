1d2236a732ccb88e61d2942ff6f4d9fc9777078e
Brycen Ong <brycenong321@gmail.com>
2025-01-28 21:44:55 +0800

    updated pokemon service

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { Pokemon } from '../shared/interfaces/pokemon.interface';
+
+interface PokeApiResponse {
+  abilities: PokeApiAbility[];
+  base_experience: number;
+  cries: {
+    latest: string;
+    legacy: string;
+  };
+  forms: {
+    name: string;
+    url: string;
+  }[];
+  name: string;
+  types: PokeApiType[];
+}
+
+interface PokeApiAbility {
+  ability: {
+    name: string,
+    url: string,
+  };
+  is_hidden: boolean;
+  slot: number;
+}
+
+interface PokeApiType {
+  slot: number;
+  type: {
+    name: string,
+    url: string,
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +44,37 @@ export class PokemonService {
     private http: HttpClient
   ) { }
 
-  getPokemonData(pokemon: string):Observable<any> {
+  getPokemonData(pokemon: string): Observable<any> {
     const cleanedPokemon = pokemon.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${cleanedPokemon}/`;
-    return this.http.get(url);
+    return this.http.get(url).pipe(
+      map((data: Partial<PokeApiResponse>) => {
+        const abilities  = this.getAbilities(data.abilities);
+        const types = this.getTypes(data.types);
+
+        return {
+          abilities,
+          types,
+        }
+      })
+    )
+  }
+
+  getAbilities(abilities: PokeApiAbility[] | undefined): string[] {
+    const abilityStrings: string[] = [];
+    abilities?.forEach((ability) => {
+      abilityStrings.push(ability.ability.name);
+    });
+
+    return abilityStrings;
+  }
+
+  getTypes(types: PokeApiType[] | undefined): string[] {
+    const typeStrings: string[] = [];
+    types?.forEach((type) => {
+      typeStrings.push(type.type.name);
+    });
+
+    return typeStrings;
   }
 }

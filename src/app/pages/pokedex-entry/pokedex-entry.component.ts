8641bec3f8faabe7c21890f0d037e87a4cff9d8b
Brycen Ong <brycenong321@gmail.com>
2025-02-11 18:26:54 +0800

    update pokedex-entry styles

--- src/app/pages/pokedex-entry/pokedex-entry.component.ts
+++ src/app/pages/pokedex-entry/pokedex-entry.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, Subscription } from 'rxjs';
 import { PokedexService } from '../../services/pokedex/pokedex.service';
 import { Pokedex } from '../../shared/interfaces/pokemon.interface';
 import { PokeApi } from '../../shared/interfaces/pokeapi.interface';
@@ -19,7 +19,7 @@ import { EvolutionTreeComponent } from "../../components/evolution-tree/evolutio
   templateUrl: './pokedex-entry.component.html',
   styleUrl: './pokedex-entry.component.scss'
 })
-export class PokedexEntryComponent implements OnInit {
+export class PokedexEntryComponent implements OnInit, OnDestroy {
   pokemonQuery: string | null = '';
   pokemon: Pokedex.Pokemon = {};
   sprites: (string | undefined)[] = [];
@@ -32,6 +32,8 @@ export class PokedexEntryComponent implements OnInit {
     spd: 'calc((255 / 255) * 100%)',
     spe: 'calc((255 / 255) * 100%)',
   }
+  routerSub?: Subscription;
+  hasEvolutions?: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -39,12 +41,19 @@ export class PokedexEntryComponent implements OnInit {
   ) {}
 
   async ngOnInit(): Promise<void> {
-    this.pokemonQuery = this.route.snapshot.paramMap.get('id');
-    this.pokemon = await firstValueFrom(this.pokedexService.getPokemonData(this.pokemonQuery as string));
-    this.sprites = Object.values(this.pokemon.sprites as PokeApi.PokemonSprites).filter((value) => typeof value === 'string');
-    const backUpImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.pokemon.id}.png`
-    this.pokemonImage = this.pokemon.sprites?.other['official-artwork'].front_default ?? backUpImage;
-    this.getPokemonStatWidths();
+    this.routerSub = this.route.params.subscribe(async () => {
+      this.pokemonQuery = this.route.snapshot.paramMap.get('id');
+      this.pokemon = await firstValueFrom(this.pokedexService.getPokemonData(this.pokemonQuery as string));
+      this.sprites = Object.values(this.pokemon.sprites as PokeApi.PokemonSprites).filter((value) => typeof value === 'string');
+      const backUpImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.pokemon.id}.png`
+      this.pokemonImage = this.pokemon.sprites?.other['official-artwork'].front_default ?? backUpImage;
+      this.getPokemonStatWidths();
+      this.hasEvolutions = ((this.pokemon.evolutionTree as Pokedex.Pokemon).evolutionTree as Pokedex.Pokemon[]).length > 0;
+    })
+  }
+
+  async ngOnDestroy(): Promise<void> {
+    this.routerSub?.unsubscribe();
   }
 
   getPokemonStatWidths(): void {

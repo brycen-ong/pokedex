6f0c0429a4b96f0a97a237521f881a0e251b777e
Brycen Ong <brycenong321@gmail.com>
2025-02-11 15:40:12 +0800

    added pages/ to folder structure, moved shared/components to components/

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { PokedexEntryComponent } from './components/pokedex-entry/pokedex-entry.component';
-import { PokedexComponent } from './components/pokedex/pokedex.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PokedexEntryComponent } from './pages/pokedex-entry/pokedex-entry.component';
+import { PokedexComponent } from './pages/pokedex/pokedex.component';
 import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
 import { EmptyLayoutComponent } from './layouts/empty-layout/empty-layout.component';
 

25b0ecbed1a67a254c98caa0801b16f8185040d5
Brycen <brycenong321@gmail.com>
2025-02-19 02:18:11 +0800

    updated pokemon search card

--- src/app/components/pokedex-search-card/pokedex-search-card.component.ts
+++ src/app/components/pokedex-search-card/pokedex-search-card.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { PokeApi } from '../../shared/interfaces/pokeapi.interface';
+import { Pokedex } from '../../shared/interfaces/pokemon.interface';
+import { PokedexService } from '../../services/pokedex/pokedex.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-pokedex-search-card',
-  imports: [],
+  imports: [
+    MatCardModule,
+  ],
   templateUrl: './pokedex-search-card.component.html',
   styleUrl: './pokedex-search-card.component.scss'
 })
-export class PokedexSearchCardComponent {
+export class PokedexSearchCardComponent implements OnChanges {
+  @Input() pokemonResource?: PokeApi.NamedAPIResource;
+  pokemon?: Pokedex.Pokemon;
+  pokemonImage?: string;
+  pokemonPrimaryType?: string;
 
+  constructor(
+    private pokedexService: PokedexService,
+  ) {}
+  
+  async ngOnChanges(changes: SimpleChanges): Promise<void> {
+    if (changes['pokemonResource']) {
+      this.pokemon = await firstValueFrom(this.pokedexService.getPokemonData(this.pokemonResource?.name as string));
+      console.log(this.pokemon.sprites);
+      this.pokemonImage = this.pokemon?.sprites?.front_default;
+      this.pokemonPrimaryType = this.pokemon?.types?.[0];
+    }
+  }
+
+  extractId(url: string): number {
+    return parseInt(url.replace('https://', '').split('/')[4]);
+  }
 }

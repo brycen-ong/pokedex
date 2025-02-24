3363eaab7848e7b73db98484397259b2e9535045
Brycen <brycenong321@gmail.com>
2025-02-25 02:01:06 +0800

    made debounce decorator more readable

--- src/app/shared/decorators/debounce.decorator.ts
+++ src/app/shared/decorators/debounce.decorator.ts
@@ -1,15 +1,13 @@
-export function Debounce(delay: number = 300): any {
+export function Debounce(delay: number = 100): any {
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    console.log('debounce');
-    console.log(descriptor.value);
-    console.log(propertyKey);
-
-    const original = descriptor.value;
-    const key = `__timeout__${propertyKey}`;
+    const originalFunc: Function = descriptor.value; // original onScroll() function
+    const timeOutVar = `${propertyKey}`; // variable to be created inside the InfiniteScrollDirective context
 
     descriptor.value = function (...args: any) {
-      clearTimeout(this[key as keyof PropertyDescriptor]);
-      this[key as keyof PropertyDescriptor] = setTimeout(() => original.apply(this, args), delay);
+      // clear the variable every time the function is called (restart the debounce timer)
+      clearTimeout(this[timeOutVar as keyof PropertyDescriptor]);
+      // set a timeout for the function call of the original function
+      this[timeOutVar as keyof PropertyDescriptor] = setTimeout(() => originalFunc.apply(this, args), delay);
     };
 
     return descriptor;

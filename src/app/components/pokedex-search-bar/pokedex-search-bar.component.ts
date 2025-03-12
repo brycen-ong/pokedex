06f67504832803409606a1ce0f60d19987f82b55
Brycen <brycenong321@gmail.com>
2025-03-12 13:49:34 +0800

    updated search bar behavior

--- src/app/components/pokedex-search-bar/pokedex-search-bar.component.ts
+++ src/app/components/pokedex-search-bar/pokedex-search-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Debounce } from '../../shared/decorators/debounce.decorator';
 
 @Component({
   selector: 'app-pokedex-search-bar',
@@ -18,6 +19,7 @@ export class PokedexSearchBarComponent {
   @Output() searchEvent = new EventEmitter<string | null>();
   searchKey = new FormControl('');
 
+  @Debounce(500)
   search() {
     this.searchEvent.emit(this.searchKey.value);
   }

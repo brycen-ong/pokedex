b7d96a4d5569ca89e93de6d208e0fb0ce5250014
Brycen <brycenong321@gmail.com>
2025-03-12 13:50:02 +0800

    added support for searching

--- src/app/pages/pokedex/pokedex.component.ts
+++ src/app/pages/pokedex/pokedex.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { PokedexSearchBarComponent } from "../../components/pokedex-search-bar/pokedex-search-bar.component";
 import { PokedexService } from '../../services/pokedex/pokedex.service';
 import { PokeApi } from '../../shared/interfaces/pokeapi.interface';
-import { firstValueFrom } from 'rxjs';
 import { PokedexSearchCardComponent } from '../../components/pokedex-search-card/pokedex-search-card.component';
 import { InfiniteScrollDirective } from '../../shared/directives/infinite-scroll/infinite-scroll.directive';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -21,45 +19,53 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 })
 export class PokedexComponent implements OnInit {
   allPokemon?: PokeApi.NamedAPIResource[] = [];
+  paginatedPokemon?: PokeApi.NamedAPIResource[] = [];
   isLoading: boolean = false;
-  offset: number = 0;
-  limit: number = 360;
   atLimit: boolean = false;
+  offset: number = 0;
+  limit: number = 120;
 
   constructor(
-    private router: Router,
-    private route: ActivatedRoute,
     private pokedexService: PokedexService,
   ) {}
 
-  async ngOnInit(): Promise<void> {
-    await this.getPokemonDataPaginated();
+  ngOnInit(): void {
+    this.getPokemonDataPaginated();
   }
 
-  async searchPokemon(searchKey: string | null): Promise<void> {
-    if (searchKey) {
-      this.router.navigate([`./${searchKey.toLocaleLowerCase()}`], { relativeTo: this.route });
-    }
-  }
-
-  async getPokemonDataPaginated(): Promise<void> {
-    if (this.atLimit) return;
-    if (this.offset + this.limit > 1025) {
-      this.limit = 1025 - this.offset;
-    }
+  searchPokemon(searchKey: string | null): void {
     this.isLoading = true;
-    const { results } = await firstValueFrom(this.pokedexService.getAllPokemonDataPaginated(this.offset, this.limit));
-    this.allPokemon?.push(...results);
-    this.offset += this.limit;
-    if (this.offset >= 1025) {
+    if (searchKey) {
+      this.allPokemon = this.pokedexService.searchPokemonData(searchKey as string);
       this.atLimit = true;
+    } else {
+      this.allPokemon = this.paginatedPokemon;
+      this.checkAtLimit();
     }
     this.isLoading = false;
   }
 
-  async loadNext(event: any) {
-    if (event === 'end') {
-      await this.getPokemonDataPaginated();
+  getPokemonDataPaginated(): void {
+    if (this.atLimit) return;
+    this.isLoading = true;
+    if (this.offset + this.limit > 1025) {
+      this.limit = 1025 - this.offset;
     }
+    const results = this.pokedexService.getAllPokemonDataPaginated(this.offset, this.limit);
+    this.paginatedPokemon?.push(...results);
+    this.allPokemon = this.paginatedPokemon;
+    this.offset += this.limit;
+    this.checkAtLimit();
+    this.isLoading = false;
+  }
+
+  loadNext(event: any): void {
+    if (event === 'end') {
+      this.getPokemonDataPaginated();
+    }
+  }
+
+  private checkAtLimit(): void {
+    this.offset >= 1025 ? this.atLimit = true : this.atLimit = false;
   }
 }

a9354bae27e392a86d459eab19f699c60922abe9
Brycen Ong <brycenong321@gmail.com>
2025-03-12 18:24:17 +0800

    updated interfaces

--- src/app/shared/interfaces/pokedex.interface.ts
+++ src/app/shared/interfaces/pokedex.interface.ts
@@ -15,11 +15,23 @@ export namespace Pokedex {
     types?: string[];
     abilities?: string[];
     hidden_abilities?: string[];
+    moves?: PokeApi.NamedAPIResource[];
     height?: number;
     weight?: number;
     sprites?: PokeApi.PokemonSprites;
     flavorText?: string;
     baseExp?: number;
     evolutionTree?: string | Pokemon | Pokemon[];
+  }
+  
+  export interface Move {
+    id?: number;
+    name?: string;
+    type?: string;
+    damage_class?: string;
+    power?: number;
+    pp?: number;
+    accuracy?: number;
+    priority?: number;
   }
 }
